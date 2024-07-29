adac71dd18de5bb7c238f2591118f44b85ae5218
nwilkie22 <nick.a.wilkie@gmail.com>
2024-07-29 14:30:57 -0400

    Rubiks Cube Object Created + it can draw a cube face

--- RubiksCube.py
+++ RubiksCube.py
@@ -15,14 +15,30 @@ class RubiksCube(pygame.sprite.Sprite):
         self.xpos = xpos
         self.ypos = ypos
         self.size = size
-        self.face = ["up", "down", "left", "right", "front", "back"]
+        # stores all the face objects in this order
+        # "up", "down", "left", "right", "front", "back"
+        self.faces = []
 
 
 
     def draw(self, screen):
+        # we need to loop this 6 times changing the position as required for the six faces
+        face = Face(self.xpos, self.ypos, self.size)
+        self.faces.append(face)
+        face.drawFace(screen)
+
+
+class Face(pygame.sprite.Sprite):
+    def __init__(self, xpos, ypos, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.xpos = xpos
+        self.ypos = ypos
+        self.size = size
+
+    def drawFace(self, screen):
         # draws a face
         for i in range(3):
             for j in range(3):
-                rect = pygame.Rect(self.xpos + i * self.size, self.ypos + j * self.size,self.size,self.size)
+                rect = pygame.Rect(self.xpos + i * self.size, self.ypos + j * self.size, self.size, self.size)
                 pygame.draw.rect(screen, RED, rect)
                 pygame.draw.rect(screen, (0, 0, 0), rect, 1)
