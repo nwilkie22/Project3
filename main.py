dc049b3f2b09f6d1e4959b660a4dfa96c583b9ce
nwilkie22 <nick.a.wilkie@gmail.com>
2024-07-29 13:41:06 -0400

    Screen Added

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # Rubiks Cube Project
+import pygame
+
+background_color = (255, 255, 255)
+
+screen = pygame.display.set_mode((1000, 1000))
+pygame.display.set_caption('Rubiks Cube Solver')
+
+screen.fill(background_color)
+
+pygame.display.update()
+
+active = True
+while active:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            active = False
